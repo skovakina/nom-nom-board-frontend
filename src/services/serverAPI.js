07c87fe248c3ac65417bfbc69954df2d40a75935
Svetlana Kovakina <skovakina@gmail.com>
2025-04-05 10:35:28 -0700

    feat: Set BASE_URL with fallback to localhost

---
+++ src/services/serverAPI.js
@@ -0,0 +1,2 @@
+export const BASE_URL =
+  import.meta.env.VITE_BACK_END_SERVER_URL || "http://localhost:3000";
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,11 +1,11 @@
-const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/users`;
+import { BASE_URL } from "./serverAPI.js";
 
 const index = async () => {
   try {
     const res = await fetch(BASE_URL, {
       headers: {
-        'Authorization': `Bearer ${localStorage.getItem('token')}`
-      }
+        Authorization: `Bearer ${localStorage.getItem("token")}`,
+      },
     });
 
     const data = await res.json();
@@ -20,7 +20,4 @@ const index = async () => {
   }
 };
 
-
-export {
-  index,
-};
+export { index };
