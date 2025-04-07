9640218af8a2ffcfa57d6cb90f9ac4ac631c78a4
clairecleverlamb <clairechenszeying@gmail.com>
2025-04-07 14:36:46 -0700

    wip: style landing page

--- src/components/Landing/Landing.jsx
+++ src/components/Landing/Landing.jsx
@@ -21,7 +21,6 @@ const Landing = () => {
           Meal Management for Busy Bee
         </h1>
 
-        {/* Subtitle */}
         <p
           className="text-[16px] md:text-[20px] font-poppins font-medium leading-none text-gray-600 text-center w-full md:w-[696px] h-auto md:h-[90px] mb-4"
         >
@@ -29,14 +28,12 @@ const Landing = () => {
           makes planning, prepping, and reflecting on your meals fun and visual.
         </p>
 
-        {/* Get Started - Free forever button */}
         <Button
           className="py-2 px-4 gap-[8px] rounded-[6px] border border-black bg-black text-white font-poppins font-medium mb-4"
         >
           Get Started - Free forever →
         </Button>
 
-        {/* Text below the button */}
         <p
           className="text-[12px] md:text-[14px] font-poppins font-medium leading-none text-gray-500 text-center w-full md:w-[216px] h-auto md:h-[42px]"
         >
@@ -46,7 +43,7 @@ const Landing = () => {
 
       {/* Placeholder for the screenshot in the bottom half */}
       <div className="w-full md:w-[696px] h-[50vh] mt-auto mb-8 flex items-center justify-center">
-        {/* Replace this with our screenshot */}
+        {/* Replace this with our screenshot later */}
         <div className="w-full h-full bg-gray-300 flex items-center justify-center rounded-lg">
           <p className="text-gray-600">Screenshot Placeholder</p>
         </div>
