6e4722f781b5110305201e8cca242a3ce8d5b6e1
clairecleverlamb <clairechenszeying@gmail.com>
2025-04-08 14:50:43 -0700

    wip: added setting routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import { useContext } from "react";
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import NavBar from "./components/NavBar/NavBar";
 import SignUpForm from "./components/SignUpForm/SignUpForm";
 import SignInForm from "./components/SignInForm/SignInForm";
 import Landing from "./components/Landing/Landing";
 import Dashboard from "./components/Dashboard/Dashboard";
+import Settings from "./components/Settings/Settings";
 import { UserContext } from "./contexts/UserContext";
 
 // this is how you import shadcn ui https://ui.shadcn.com/docs/components/button
@@ -23,6 +24,10 @@ function App() {
         <Route path="/" element={user ? <Dashboard /> : <Landing />} />
         <Route path="/sign-up" element={<SignUpForm />} />
         <Route path="/sign-in" element={<SignInForm />} />
+        <Route
+          path="/settings"
+          element={user ? <Settings /> : <Navigate to="/sign-in" />}
+        />
       </Routes>
     </>
   );
