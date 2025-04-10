9c742ea2b41be132005d83fb97216e885c17d5cd
tsztin0217 <tsztin0217@gmail.com>
2025-04-10 15:32:13 -0400

    Render images in carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -10,45 +10,73 @@ import Autoplay from "embla-carousel-autoplay";
 
 
 const CarouselComponent = () => {
+
     const reviews = [
-        { review: "Efficient and Easy to Use!", user: "John D." },
-        { review: "Perfect for Healthy Eating!", user: "Jane S." },
-        { review: "Convenient and Time-Saving!", user: "Alice J." },
-        { review: "My Go-To App for Meal Planning!", user: "Bob F." },
-        { review: "Meal Planning Made Fun!", user: "Charlie K." },
+        {
+            review: "Efficient and Easy to Use!",
+            user: "John D.",
+            image: "/images/1.png"
+        },
+        {
+            review: "Perfect for Healthy Eating!",
+            user: "Jane S.",
+            image: "/images/2.png"
+        },
+        {
+            review: "Convenient and Time-Saving!",
+            user: "Alice J.",
+            image: "/images/3.png"
+        },
+        {
+            review: "My Go-To App for Meal Planning!",
+            user: "Bob F.",
+            image: "/images/4.png"
+        },
+        {
+            review: "Meal Planning Made Fun!",
+            user: "Charlie K.",
+            image: "/images/5.png"
+        },
     ];
 
     return (
+        <>
+            <Carousel
+                opts={{
+                    align: "start",
+                }}
+                plugins={[
+                    Autoplay({
+                        delay: 5000,
+                    }),
+                ]}
+            >
+                <CarouselContent className="-ml-1 w-full">
+                    {reviews.map((reviewObj, index) => (
+                        <CarouselItem key={index} className="w-full">
+                            <div className="p-1">
+                                <Card className="flex h-full justify-center items-center">
+                                    <CardContent className="flex flex-col justify-center items-center p-6 text-center">
+                                        <div className="relative bg-muted w-full rounded overflow-hidden">
+                                            <img
+                                                src={reviewObj.image}
+                                                alt={`Review by ${reviewObj.user}`}
+                                                className="w-full h-full object-cover"
+                                            />
+                                        </div>
+                                        <span className="text-xl" style={{ fontSize: '80%' }}>{reviewObj.review}</span>
+                                        <div className="mt-1">
+                                            <span className="font-bold text-lg" style={{ fontSize: '60%' }}>- {reviewObj.user}</span>
+                                        </div>
+                                    </CardContent>
+                                </Card>
+                            </div>
+                        </CarouselItem>
+                    ))}
+                </CarouselContent>
 
-        <Carousel
-            opts={{
-                align: "start", 
-            }}
-            plugins={[
-                Autoplay({
-                    delay: 3000,
-                }),
-            ]}
-        >
-            <CarouselContent className="-ml-1">
-                {reviews.map((reviewObj, index) => (
-                    <CarouselItem key={index} className="pl-1 md:basis-1/2 lg:basis-1/3">
-                        <div className="p-1">
-                            <Card className="h-30">
-                                <CardContent className="flex flex-col justify-center items-center p-6 h-full text-center">
-                                    <span className="text-xl" style={{ fontSize: '80%' }}>{reviewObj.review}</span>
-                                    <div className="mt-1">
-                                        <span className="font-bold text-lg" style={{ fontSize: '60%' }}>- {reviewObj.user}</span>
-                                    </div>
-                                </CardContent>
-                            </Card>
-
-                        </div>
-                    </CarouselItem>
-                ))}
-            </CarouselContent>
-
-        </Carousel>
+            </Carousel>
+        </>
     )
 }
 
